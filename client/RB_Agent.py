2ca4679a45656bda707b2af3d5dc720713ccf1a0
hsuw3 <willh7805@gmail.com>
2018-03-25 16:00:44 -0400

    Add files via upload

---
+++ client/RB_Agent.py
@@ -0,0 +1,280 @@
+#Agent Class
+import networkx as nx
+import sys
+import matplotlib.pyplot as plt
+import RB_Graph as G
+import random
+import copy
+
+#TODO: need to fix return type from node to string of number
+#MiniMax has to be in function type not class type
+
+"""
+" This is the file one needs to modify.
+" One will be able to write there own algorithm under costom_algorithem.
+" Feel free to make multiple costume Algorithm.
+" Parameter for the algorithem are graph and player.
+" Graph is RB_Graph format and player is in string.
+" Player will be either "red" or "blue"
+" Return type has to be node of graph.
+" If there are multiple Algorithms that has been added, 
+" run" function under "computer" class must be modified accordingly.
+"""
+
+"""
+Random algorithm that makes a move randomly
+"""
+def RandomAlgorithm(g, player):
+    return str(random.choice(g.getValidMoves()))
+"""
+Simple Greedy Algorithm that looks for the node with the most amount of neighboring nodes.
+"""
+def GreedyNodesAlgorithm(g, player):
+    valid_moves = g.getValidMoves()
+    chosen_moves = []
+    max_degree = -1
+    for node in valid_moves:
+        degree = 0
+        for neighbors in g.graph.neighbors(node):
+            degree += 1
+        if max_degree < degree:
+            chosen_moves = []
+            chosen_moves.append(node)
+            max_degree = degree
+        elif max_degree == degree:
+            chosen_moves.append(node)
+    return str(random.choice(chosen_moves))
+
+"""
+Greedy Algorithm that considers the amount of nodes gained and the amount of nodes stolen from the opponent
+"""
+def GreedyPointsAlgorithm(g, player):
+    valid_moves = g.getValidMoves()
+    chosen_moves = []
+    gamestate = nx.get_node_attributes(g.graph, 'color')
+    max_degree = -1
+    for node in valid_moves:
+        degree = 0
+        for neighbors in g.graph.neighbors(node):
+            if player == 'red':
+                if gamestate.get(neighbors) == 'grey':
+                    degree += 1
+                elif gamestate.get(neighbors) == 'blue':
+                    degree += 2
+            else:
+                if gamestate.get(neighbors) == 'grey':
+                    degree += 1
+                elif gamestate.get(neighbors) == 'red':
+                    degree += 2
+        if max_degree < degree:
+            chosen_moves = []
+            chosen_moves.append(node)
+            max_degree = degree
+        elif max_degree == degree:
+            chosen_moves.append(node)
+    return str(random.choice(chosen_moves))
+"""
+Minimax algorithm
+"""
+def MinimaxAlgorithm(g, player):
+    m = Minimax(1)
+    return str(m.getAction(g,player))
+
+class Minimax():
+    def __init__(self, depth):
+        self.index = 0
+        self.player = None
+        self.enemy = None
+        self.neighbors = None
+        self.depth = int(depth)
+
+    def getAction(self, g, player):
+        current_depth = 0
+        current_agent = 0
+        value = -float('inf')
+        self.player = player
+        if player == 'red':
+            self.enemy = 'blue'
+        else:
+            self.enemy = 'red'
+        gamestate = nx.get_node_attributes(g.graph, 'color')
+        valid_moves = set(g.getValidMoves())
+
+        neighbors = []
+        for node in range(g.nodeNum):
+            adj = []
+            for neighbor in g.graph.neighbors(node):
+                adj.append(neighbor)
+            neighbors.append(adj)
+        self.neighbors = neighbors
+        chosen_moves = []
+        gameinfo = [gamestate, valid_moves, current_agent]
+        for move in valid_moves:
+            #print(str(move))
+            new_value = self.value(self.generateSuccessor(move,gameinfo), current_agent + 1, current_depth)
+            #print(str(move) + " move and value " + str(new_value))
+            if value < new_value:
+                chosen_moves = []
+                chosen_moves.append(move)
+                value = new_value
+            elif value == new_value:
+                chosen_moves.append(move)
+        return random.choice(chosen_moves)
+
+
+    def value(self, gameinfo, current_agent, current_depth):
+        if current_agent > 1:
+            current_depth += 1
+            current_agent = 0
+        if current_depth >= self.depth or len(gameinfo[1]) == 0:
+            return self.evaluate(gameinfo[0])
+
+        if current_agent == 0:
+            return self.max_value(gameinfo, current_agent, current_depth)
+        else:
+            return self.min_value(gameinfo, current_agent, current_depth)
+
+    def max_value(self, gameinfo,current_agent, current_depth):
+        if current_depth >= self.depth or len(gameinfo[1]) == 0:
+            return self.evaluate(gameinfo[0])
+        value = -float("inf")
+        valid_moves = gameinfo[1]
+        for move in valid_moves:
+            new_value = self.value(self.generateSuccessor(move,gameinfo), current_agent + 1, current_depth)
+            if value < new_value:
+                value = new_value
+        return value
+
+    def min_value(self, gameinfo, current_agent, current_depth):
+        if current_depth >= self.depth or len(gameinfo[1]) == 0:
+            return self.evaluate(gameinfo[0])
+        value = float("inf")
+        valid_moves = gameinfo[1]
+        for move in valid_moves:
+            new_value = self.value(self.generateSuccessor(move,gameinfo), current_agent + 1, current_depth)
+            if value > new_value:
+                value = new_value
+        return value
+
+    def evaluate(self, gamestate):
+        bluecount = 0
+        redcount = 0
+        for node in gamestate:
+            if(gamestate.get(node) == 'red'):
+                redcount += 1
+            if(gamestate.get(node) == 'blue'):
+                bluecount += 1
+        if self.player == 'red':
+            return redcount - bluecount
+        else:
+            return bluecount - redcount
+    def generateSuccessor(self, move, gameinfo):
+        gamestate = copy.deepcopy(gameinfo[0])
+        valid_moves = set(gameinfo[1])
+        agent = int(gameinfo[2])
+        if (agent %2 == 0):
+            player = self.player
+        else:
+            player = self.enemy
+        gamestate[move] = player
+        valid_moves.remove(move)
+        for node in self.neighbors[move]:
+            if (gamestate[node] == 'grey'):
+                valid_moves.remove(node)
+            gamestate[node] = player
+            return [gamestate, valid_moves, agent+1]
+  
+
+
+
+
+
+def CustomAlgorithm(g,player):
+    """
+    colors are dictionary of dictionary.
+    Key is a node number and value is a color of the node.
+    You can access your color by self.getColor()
+    return string of integer by using str() function
+    check the given algorithms (random,greedy) for references..
+    """
+    colors = nx.get_node_attributes(g.get_graph(),'color')
+    #Todo: Write your code here!
+    return 
+"""
+Agent types
+"""
+class human(object):
+    def __init__(self,color):
+        self.color = color
+    
+    def getColor(self):
+        return self.color
+    
+    def run(self,graph,color):
+        return raw_input()
+
+class computer(object):
+    def __init__(self,color):
+        self.color = color
+        self.algorithm = Minimax(5)
+        self.makeSelection = False
+    
+    def getColor(self):
+        return self.color
+    
+    def run(self,graph,color):
+        #write which algo to run
+        """
+        change it to your algorithm you want to run
+        """
+        if self.makeSelection:
+            return self.algorithm(graph,color)
+        else:
+            print "Choose algorithm to use:"
+            print "1. Random selection"
+            print "2. Greedy Nodes Algorithm"
+            print "3. Greedy Points Algorithm"
+            print "4. Minimax Algorithm"
+            #print "5. Custom Algorithm"
+            """
+            YOU MAY NEED TO ADD MORE PRINT
+            """
+            #print "6. Custom Algorithm2"
+            #print "7. Custom Algorithm3"
+            #print "8. Custom Algorithm4"
+            #...
+            valid_choice = False
+            while(not valid_choice):
+                valid_choice = True
+                select = raw_input()
+                if select.isdigit():
+                    select = int(select)
+                if select == 1:
+                    self.algorithm = RandomAlgorithm
+                elif select == 2 :
+                    self.algorithm = GreedyNodesAlgorithm
+                elif select == 3 :
+                    self.algorithm = GreedyPointsAlgorithm
+                elif select == 4 :
+                    self.algorithm = MinimaxAlgorithm
+                # elif select == 5 :
+                #     self.algorithm = CustomAlgorithm
+                else:
+                    valid_choice = False
+                    print("Invalid choice of algorithm")
+            """
+            YOU MAY NEED TO ADD MORE elif statement
+            """
+            #elif select == 6 :
+            #    self.algorithm = CustomAlgorithm2
+            #elif select == 7 :
+            #    self.algorithm = CustomAlgorithm3
+            #elif select == 8 :
+            #    self.algorithm = CustomAlgorithm4
+            #elif select == 9 :
+            #    self.algorithm = CustomAlgorithm5
+            self.makeSelection = True
+            return self.algorithm(graph,color)
+        
+if __name__=="__main__":
+    graph = G.get_graph()
---
+++ client/RB_Graph.py
@@ -0,0 +1,236 @@
+"""
+<Red and Blue game>
+Author: Jihoo Brian Park, William Hsu
+Class: Graph Class
+Discription: This generates Erdos-Renyi graph.
+Default color is grey. when nodes are marked by player, graph updates the node color.
+"""
+import networkx as nx
+import sys
+import numpy
+import matplotlib.pyplot as plt
+#TODO
+"""
+constant graph showing
+2D table
+"""
+class Graph(object):
+  
+    def __init__(self, nodeNum, prob):
+        self.nodeNum = nodeNum
+        self.graph = nx.erdos_renyi_graph(nodeNum,prob)
+        self.pos=nx.spring_layout(self.graph)
+        #set node attribute 
+        self.attrs = {}
+        for num in range(self.nodeNum):
+            self.attrs[num] = {'number':num,'color':'grey'}
+        nx.set_node_attributes(self.graph,self.attrs)
+
+    def getValidMoves(self):
+        """
+        Get the list of nodes that have not been colored yet.
+        :return: a list of node numbers that are available to be chosen as the next move
+        """
+        valid_moves = []
+        gamestate = nx.get_node_attributes(self.graph, 'color')
+        for node in gamestate:
+            if gamestate.get(node) == 'grey':
+                valid_moves.append(node)
+
+        return valid_moves
+
+    def readGraph(self, graph_data):
+        self.graph.clear()
+        nodes = []
+        edges = []
+        num_red = 0
+        num_blue = 0
+        self.attrs = {}
+        for x in range(len(graph_data["graph"])):
+            nodes.append(x)
+            self.attrs[x] = {'number': x, 'color':graph_data["colors"][x] }
+            for adj in range(len(graph_data["graph"][x])):
+                edges.append((x,graph_data["graph"][x][adj]))
+
+        for node in self.attrs.values():
+            node_color = ''
+            color_num = node.get('color')
+            if(color_num == 0):
+                node_color = 'grey'
+            if(color_num == 1):
+                num_red += 1
+                node_color = 'red'
+            if(color_num == 2):
+                num_blue += 1
+                node_color = 'blue'
+            node['color'] = node_color
+        self.graph.add_nodes_from(nodes)
+        self.graph.add_edges_from(edges)
+        self.pos = nx.spring_layout(self.graph)
+        nx.set_node_attributes(self.graph,self.attrs)
+        return(num_red, num_blue)
+
+    def printGraphDetails(self, game, file):
+        file.write('If you want to play again using the same graph, use the contents of "game_graph.json" file as an input file\n\n')
+        file.write("Game History: \n\n")
+        file.write("Total Turns: " + str(len(game.move_list[0]) + len(game.move_list[1])) + "  \t\tScore:\t(Red, Blue)\n\n")
+        redmove = 0
+        bluemove = 0
+        for round in range(len(game.move_list[0]) + len(game.move_list[1])):
+            if (round % 2 == 0):
+                file.write(
+                    "Round " + str(round/2 + 1) + ':\t Red Chose  ' + str(game.move_list[0][redmove]) + '    \t' + str(
+                        game.score_list[round]) + '\n')
+                redmove += 1
+            else:
+                file.write(
+                    "       " + ' \t Blue Chose ' + str(game.move_list[1][bluemove]) + '    \t' + str(
+                        game.score_list[round]) + '\n')
+                bluemove += 1
+        file.write('\n\nGame Results: \n\n')
+        red_nodes = []
+        blue_nodes = []
+        grey_nodes = []
+        for node in range(self.nodeNum):
+            color = self.attrs[node].get('color')
+            if(color == "grey"):
+                grey_nodes.append(node)
+            elif(color == "red"):
+                red_nodes.append(node)
+            else:
+                blue_nodes.append(node)
+        file.write("Red has " + str(game.point[0]) + " points \n")
+        file.write("Blue has " + str(game.point[1]) + " points \n")
+        file.write("Red nodes: " + str(red_nodes) + "\n")
+        file.write("Blue nodes: " + str(blue_nodes) + "\n")
+        file.write("Neutral nodes: " + str(grey_nodes))
+        file.write('\n\n\nGame Graph: \n\nTotal number of nodes: \n' + str(self.nodeNum) + '\n')
+        for node in range(self.nodeNum):
+            adj = []
+            for neighbor in self.graph.neighbors(node):
+                adj.append(neighbor)
+            file.write("node: " + str(node) + " \tneighbors: " + str(adj) + "\n")
+        file.close()
+
+    def printGraphJson(self, game, file):
+        file.write("{\n")
+        file.write('\t"rounds": '+ str(game.rounds) + ",\n")
+        file.write('\t"time": ' + str(game.time) + ",\n")
+        file.write('\t"graph": [\n')
+        first1 = True
+        for node in range(self.nodeNum):
+            if(first1):
+                file.write('\t\t[')
+                first1 =False
+            else:
+                file.write(',\n\t\t[')
+            first = True
+            for neighbor in self.graph.neighbors(node):
+                if(first):
+                    file.write(str(neighbor))
+                    first = False
+                else:
+                    file.write(', ' + str(neighbor))
+            file.write(']')
+        file.write('\n\t],\n')
+        file.write('\t"colors": [')
+        first = True
+        for node in range(self.nodeNum):
+            if(first):
+                file.write(str(0))
+                first = False
+            else:
+                file.write(', ' + str(0))
+        file.write(']\n}')
+        file.close()
+
+    def mark(self, player, node):
+        """
+        mark node that player has chosen.
+        return tuple: number of nodes added and number of node subtracted
+        """
+        colored_nodes = 0
+        subtracted_nodes = 0
+        if(node not in self.graph.node):
+            sys.stderr.write('node does not exist\n')
+            return (0, 0)
+        if self.attrs[node]['color'] == 'grey':
+            self.attrs[node]['color'] = player
+            colored_nodes+=1
+        else:
+            sys.stderr.write('node has been taken')
+            return(0,0)
+            
+        for nodes in self.graph.neighbors(node):
+            if(self.attrs[nodes]['color'] == player):
+                continue
+            if (self.attrs[nodes]['color'] != 'grey'):
+                subtracted_nodes +=1
+            self.attrs[nodes]['color'] = player
+            colored_nodes+=1
+        nx.set_node_attributes(self.graph,self.attrs)
+        return (colored_nodes,subtracted_nodes)
+
+    def printTable(self):
+        """
+        print 2D table
+        print 2D table
+        """
+        adjacency_matrix = []
+        for node in self.graph.nodes():
+            row = []
+            row2 = []
+            for neighbor in range(self.nodeNum):
+                if(neighbor in self.graph.neighbors(node)):
+                    row.append(1)
+                else:
+                    row.append(0)
+            print(row)
+            adjacency_matrix.append(row)
+        
+    def printGraph(self):
+        """
+        print Graph
+        """
+        plt.clf()
+        labels = nx.get_node_attributes(self.graph,'number')
+        colors = nx.get_node_attributes(self.graph,'color')
+        for node in colors:
+            if(colors[node] == 'red'):
+                colors[node] = 'lightcoral'
+            if(colors[node] == 'blue'):
+                colors[node] = 'skyblue'
+        nx.draw(self.graph,self.pos,node_color = colors.values(),with_lables=True,node_size = 250)
+        nx.draw_networkx_labels(self.graph,self.pos,labels,font_size=8)
+        plt.axis('off')
+        plt.show()
+    #plt.draw()
+        
+    def printGraph_node_only(self):
+        """
+        print Graph
+        """
+        plt.clf()
+        labels = nx.get_node_attributes(self.graph,'number')
+        colors = nx.get_node_attributes(self.graph,'color')
+        #print colors
+        for node in colors:
+            if(colors[node] == 'red'):
+                colors[node] = 'lightcoral'
+            if(colors[node] == 'blue'):
+                colors[node] = 'skyblue'
+        nx.draw_networkx_nodes(self.graph,self.pos,node_color = colors.values(),with_lables=True,node_size = 250)
+        nx.draw_networkx_labels(self.graph,self.pos,labels,font_size=8)
+        plt.axis('off')
+        plt.pause(0.1)    
+    def get_graph(self):
+        return self.graph
+    
+if __name__=="__main__":
+    
+    n=50 # 50 nodes
+    p=0.2 # prob
+    G = Graph(n,p)
+    print G.mark('red',1)
+    print G.mark('blue',0)
+    G.printGraph_node_only()
