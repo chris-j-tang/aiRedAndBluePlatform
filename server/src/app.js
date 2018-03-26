a8ca7be09c3b3ca60365755e33714df55070644f
Jeff Poegel <jpoegel@comcast.net>
2018-03-25 20:43:59 -0400

    Added support for server communication

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
 const assert = require('assert');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+
 const hidgen = require('human-readable-ids').hri.random;
 
 const RateLimiter = require('express-rate-limit');
@@ -10,6 +13,20 @@ const Graph = require('./graph');
 let games = {};
 
 app.use(express.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+	strict: true
+}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/', express.static(path.join(__dirname, './../../public')))
 
 app.route('/game')
   // Starting a game
@@ -26,6 +43,25 @@ app.route('/game')
 
   }), function (req, res) {
     try {
+	  console.log("Attempting to start a new game...");
+	  
+	  if(typeof req.body.nodes === 'string' || req.body.nodes instanceof String) {
+		req.body.nodes = parseInt(req.body.nodes);  
+	  }
+	  if(typeof req.body.rounds === 'string' || req.body.rounds instanceof String) {
+		req.body.rounds = parseInt(req.body.rounds);  
+	  }
+	  if(typeof req.body.time === 'string' || req.body.time instanceof String) {
+		req.body.time = parseInt(req.body.time);  
+	  }
+	  if('prob' in req.body && (typeof req.body.prob === 'string' || req.body.prob instanceof String)) {
+		req.body.prob = parseFloat(req.body.prob);  
+	  }
+	  if('seed' in req.body && (typeof req.body.seed === 'string' || req.body.seed instanceof String)) {
+		req.body.seed = parseInt(req.body.seed);  
+	  }
+	  console.log(req.body);
+	  
       assert(Number.isInteger(req.body.nodes), 'Required field "nodes" must be an integer');
       assert(Number.isInteger(req.body.rounds), 'Required field "rounds" must be an integer');
       assert(Number.isInteger(req.body.time), 'Required field "time" must be an integer');
@@ -35,11 +71,15 @@ app.route('/game')
       }
       if ('seed' in req.body)
         assert(Number.isInteger(req.body.seed), 'Optional field "seed" must be an integer');
+	  
       let id = hidgen();
       while (id in games) id = hidgen();
       games[id] = new Game(req.body.rounds, req.body.time, Graph.makeRandom(req.body.nodes, req.body.prob, req.body.seed));
+	  console.log(id);
+	  //res.json(id);
       res.location('/game/' + id).sendStatus(201);
     } catch (error) {
+	  console.log("error");
       res.status(400).send(error.stack);
     }
   })
@@ -52,6 +92,7 @@ app.route('/game/:gameId/')
   .post(function (req, res) {
     let game = games[req.params.gameId];
     let player = req.body.id;
+	console.log("Player, " + player + ", joined the game");
     if (game) {
       if (!game.hasPlayers() && !game.isPlayer(player)) {
         game.join(player);
@@ -76,7 +117,7 @@ app.route('/game/:gameId/')
       } else res.sendStatus(400);
     } else res.sendStatus(404);
   });
-
+  
 app.route('/game/:gameId/:playerId/')
   // Requesting a turn
   .get(function (req, res) {
@@ -93,5 +134,5 @@ app.route('/game/:gameId/:playerId/')
       res.status(200).send(diff);
     } else res.sendStatus(404);
   });
-
+  
 app.listen(4040, () => console.log('Listening on port 4040!'));
